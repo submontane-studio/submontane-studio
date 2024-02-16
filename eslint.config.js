9bac77de690b9f62a53c7793c88c4e38df027648
Tsuyoshi Yamashita <t.yamashita@fab-jp>
2024-02-16 23:41:50 +0900

    .vscode/settings.jsonとeslint.config.jsの変更を追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,48 @@
-import nextPlugin from '@next/eslint-plugin-next';
-import reactPlugin from 'eslint-plugin-react';
-import hooksPlugin from 'eslint-plugin-react-hooks';
-import typescriptParser from '@typescript-eslint/parser';
-import js from '@eslint/js';
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
+import typescriptParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
 
 export default [
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-  },
-  {
-    ignores: ['**/dist/**']
-  },
-  js.configs.recommended,
+	{
+		files: ["**/*.{js,jsx,ts,tsx}"],
+	},
 
-  {
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module",
-        project: "./tsconfig.json",
-      },
-    },
-  },
+	{
+		ignores: [
+			"**/dist/**",
+			"**/node_modules/**",
+			"**/.next/**",
+			"eslint.config.js",
+			"*.d.ts",
+			"next.config.js",
+		],
+	},
 
-  {
-    plugins: {
-      react: reactPlugin,
-      'react-hooks': hooksPlugin,
-      '@next/next': nextPlugin
-    },
-    rules: {
-    }
-  }
-]
+	{
+		languageOptions: {
+			parser: typescriptParser,
+			parserOptions: {
+				ecmaVersion: 2018,
+				sourceType: "module",
+				project: "./tsconfig.json",
+			},
+		},
+	},
+
+	{
+		plugins: {
+			react: reactPlugin,
+			"react-hooks": hooksPlugin,
+			"@next/next": nextPlugin,
+		},
+		rules: {
+			...reactPlugin.configs["jsx-runtime"].rules,
+			...hooksPlugin.configs.recommended.rules,
+			...nextPlugin.configs.recommended.rules,
+			...nextPlugin.configs["core-web-vitals"].rules,
+			"@next/next/no-img-element": "error",
+		},
+	},
+];
