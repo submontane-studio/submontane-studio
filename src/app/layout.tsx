b342cf398b3bd3e7b2641a7b36fdb7e09e5ab9f8
Tsuyoshi Yamashita <t.yamashita@fab-jp>
2024-02-17 21:44:03 +0900

    .vscode/settings.jsonとpackage.json、src/app/layout.tsxの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,21 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: {
-    default:`${process.env.SITE_TITLE}`,
-    template: `%s | ${process.env.SITE_TITLE}`
-  },
-  description: '',
+	title: {
+		default: `${process.env.SITE_TITLE}`,
+		template: `%s | ${process.env.SITE_TITLE}`,
+	},
+	description: "",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ja">
+			<body>{children}</body>
+		</html>
+	);
 }
