32dc171a87dc44810fa72d8153cee03bae8bf9ee
Tsuyoshi Yamashita <t.yamashita@fab.jp>
2024-02-18 19:44:29 +0900

    Navigationコンポーネントのサブページリンクをリスト形式に変更

--- src/app/component/Navigation.tsx
+++ src/app/component/Navigation.tsx
@@ -8,11 +8,18 @@ export default function Navigation() {
 				{Items.map((item) => (
 					<li key={item.slug}>
 						<Link href={item.slug}>{item.name}</Link>
-						{item.subpages?.map((subItem) => (
-							<Link key={subItem.slug} href={`${item.slug}/${subItem.slug}`}>
-								{subItem.name}
-							</Link>
-						))}
+						<ul>
+							{item.subpages?.map((subItem) => (
+								<li key={subItem.slug}>
+									<Link
+										key={subItem.slug}
+										href={`${item.slug}/${subItem.slug}`}
+									>
+										{subItem.name}
+									</Link>
+								</li>
+							))}
+						</ul>
 					</li>
 				))}
 			</ul>
