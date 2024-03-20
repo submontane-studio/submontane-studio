7c6a1f0d2a06ce59f6bf5cca758f2f77a6d07ac0
Tsuyoshi Yamashita <t.yamashita@fab.jp>
2024-03-21 01:29:14 +0900

    スタイルを修正し、アコーディオンの動作を改善しました

--- src/app/component/ServiceItem.tsx
+++ src/app/component/ServiceItem.tsx
@@ -13,6 +13,11 @@ export default function ServiceItem({
     e: React.MouseEvent<HTMLLIElement> | React.KeyboardEvent<HTMLLIElement>,
   ) => void = (e) => {
     e.currentTarget.classList.toggle("is-expanded");
+    if (e.currentTarget.getAttribute("aria-expanded") === "true") {
+      e.currentTarget.setAttribute("aria-expanded", "false");
+    } else {
+      e.currentTarget.setAttribute("aria-expanded", "true");
+    }
   };
 
   return matches ? (
@@ -28,6 +33,7 @@ export default function ServiceItem({
       className={`services-item ${slug} ${isContract ? " is-contract" : ""}`}
       onClick={isContract ? expand : undefined}
       onKeyDown={isContract ? expand : undefined}
+      aria-expanded={isContract ? "false" : undefined}
     >
       {children}
     </li>
