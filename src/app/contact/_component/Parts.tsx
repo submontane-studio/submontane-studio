ceddd0c795b05b6679ea293112c9ee1e2d3cff37
Tsuyoshi Yamashita <t.yamashita@fab.jp>
2024-04-13 20:21:37 +0900

    パーツコンポーネントの入力バリデーションを修正しました

--- src/app/contact/_component/Parts.tsx
+++ src/app/contact/_component/Parts.tsx
@@ -11,21 +11,37 @@ const validate = (e: React.FocusEvent<HTMLInputElement>) => {
   const parent = part.parentElement;
   part.setCustomValidity("");
 
-  if (part.value === "") {
+  if (part.validity.valueMissing) {
     part.classList.add("is-error");
-  }
+    if (parent?.id === "name") {
+      if (part.id === "family-name") {
+        part.setCustomValidity("姓を入力してください");
+      } else {
+        part.setCustomValidity("名を入力してください");
+      }
 
-  if (parent?.id === "name") {
-    if (part.id === "family-name") {
-      part.setCustomValidity("姓を入力してください");
-    } else {
-      part.setCustomValidity("名を入力してください");
+      parent?.insertAdjacentHTML(
+        "beforeend",
+        `<p class="is-error">${part.validationMessage}</p>`,
+      );
     }
+  } else if (part.validity.patternMismatch) {
+    part.classList.add("is-error");
+    if (parent?.id === "name") {
+      if (part.id === "family-name") {
+        part.setCustomValidity("姓の入力が不正です");
+      } else {
+        part.setCustomValidity("名の入力が不正です");
+      }
 
-    parent.insertAdjacentHTML(
-      "beforeend",
-      `<p class="is-error">${part.validationMessage}</p>`,
-    );
+      parent?.insertAdjacentHTML(
+        "beforeend",
+        `<p class="is-error">${part.validationMessage}</p>`,
+      );
+    }
+  } else {
+    part.classList.remove("is-error");
+    parent?.querySelector("p.is-error")?.remove();
   }
 };
 
