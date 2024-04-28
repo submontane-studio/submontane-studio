aad2d3b5d60de796873c87b8b44dd9e7b1368b2e
Tsuyoshi Yamashita <t.yamashita@fab-jp>
2024-04-28 20:29:21 +0900

    パーツコンポーネントの入力フォームのバリデーションメッセージを修正しました

--- src/app/contact/_component/Parts.tsx
+++ src/app/contact/_component/Parts.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import validate from "../_lib/validate";
-import validatingInput from "../_lib/validatingInput";
 
 type InputProps = {
   id: string;
@@ -29,7 +28,7 @@ export const Input = ({ id, className, onChange, ...rest }: InputProps) => {
       className={className}
       {...rest}
       onChange={onChange}
-      onBlur={validatingInput}
+      onBlur={validate}
     />
   );
 };
