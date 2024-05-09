403d6889c177b42a47ce3dbfda3585d0ec45335c
Tsuyoshi Yamashita <t.yamashita@fab.jp>
2024-05-09 20:10:48 +0900

    refactor: Remove unused dependencies and configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
 
 const nextConfig = {
   // reactStrictMode: true,
