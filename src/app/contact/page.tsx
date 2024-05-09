48734801a1abdef53b509ded52ffd204562e78a6
Tsuyoshi Yamashita <t.yamashita@fab-jp>
2024-05-10 02:43:22 +0900

    refactor: Update _middleware.ts and Contact page component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,28 +13,44 @@ export const metadata: Metadata = {
 export default function Contact() {
   return (
     <>
-      <h1>Contact</h1>
-      <form data-static-form-name="contact">
-        <div>
-          <label>
-            Name
-            <input type="text" name="name" />
-          </label>
+      <div className="inpage-heading">
+        <Header />
+        <h1>お問い合わせ</h1>
+      </div>
+      <Breadcrumb
+        items={[
+          {
+            pathname: `${process.env.SITE_URL}`,
+            title: "ホーム",
+          },
+          {
+            pathname: `${process.env.SITE_URL}contact/`,
+            title: "お問い合わせ",
+          },
+        ]}
+        className={styles.breadcrumb}
+      />
+      <main>
+        <div className={styles.intro}>
+          <p>
+            案件のご相談やその他のお問い合わせの際は、以下のフォームをご使用ください。
+            <br />
+            通常、数日でメールにてご返信いたします。
+            <br />
+            いただきました内容は、当事務所の
+            <a href="/privacy-policy" target="_blank" rel="noreferrer">
+              プライバシーポリシー
+            </a>
+            に則って適正に処理いたします。
+            <br />
+            <span className="required">必須</span>
+            は必須項目です。必ずご入力ください。
+          </p>
         </div>
-        <div>
-          <label>
-            Email
-            <input type="email" name="email" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Message
-            <textarea name="message" />
-          </label>
-        </div>
-        <button type="submit">Send!</button>
-      </form>
+        <Form />
+      </main>
+      <Conversion />
+      <Footer />
     </>
   );
 }
