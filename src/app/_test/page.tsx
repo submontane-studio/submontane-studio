162288abdeccbfb1e4714b2841940220e499aa20
Tsuyoshi Yamashita <t.yamashita@fab-jp>
2024-05-14 23:24:08 +0900

    testディレクトリをルーティングから除外・marketing/featuresを追加

--- src/app/_test/page.tsx
+++ src/app/_test/page.tsx
@@ -10,16 +10,6 @@ export default function Test() {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    const res = await fetch("/api/send", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(test),
-    });
-
-    console.log(res);
   };
   return (
     <>
--- src/app/api/send/route.ts
+++
@@ -1,9 +0,0 @@
-import { request } from "http";
-import { NextRequest, NextResponse } from "next/server";
-
-export const runtime = "edge";
-
-export const POST = async (req: NextRequest) => {
-  const requestBody = await req.text();
-  console.log(requestBody);
-};
